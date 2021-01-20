a412416565056bf1da70c18c9745b8846d8329e0
Dmitry Izmestev <dzmstv01@gmail.com>
2021-01-21 01:04:24 +0500

    server add

---
+++ src/web/server/ChatServer.java
@@ -0,0 +1,70 @@
+package web.server;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+
+public class ChatServer {
+    private final int port = 5000;
+    private ArrayList clientOutStreams;
+
+    public static void main(String[] args) {
+        new ChatServer().go();
+    }
+
+    public void go() {
+        clientOutStreams = new ArrayList();
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+                clientOutStreams.add(writer);
+
+                Thread thread = new Thread(new ClientHandler(clientSocket));
+                thread.start();
+                System.out.println("got a connection");
+            }
+        } catch (Exception ex) {ex.printStackTrace();}
+    }
+
+    private void tellEveryone(String message) {
+        Iterator iterator = clientOutStreams.iterator();
+        while (iterator.hasNext()) {
+            try {
+                PrintWriter writer = (PrintWriter) iterator.next();
+                writer.println(message);
+                writer.flush();
+            } catch (Exception ex) {ex.printStackTrace();}
+        }
+    }
+
+
+    public class ClientHandler implements Runnable {
+        BufferedReader reader;
+        Socket socket;
+
+        public ClientHandler(Socket clientSocket) {
+            try {
+                socket = clientSocket;
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            } catch (Exception ex) {ex.printStackTrace();}
+        }
+
+        @Override
+        public void run() {
+            String message;
+            try {
+                message = reader.readLine();
+                while (message != null) {
+                    System.out.println("read " + message);
+                    tellEveryone(message);
+                    message = reader.readLine();
+                }
+            } catch (Exception ex) {ex.printStackTrace();}
+        }
+    }
+
+}
--- src/web/server/chatServer.java
+++
@@ -1,4 +0,0 @@
-package web.server;
-
-public class chatServer {
-}
